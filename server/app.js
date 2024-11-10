b4b08f595114979ac2ad97cc0c10a10c3ed8398e
Geoffrey Gracia <grg7576@rit.edu>
2024-11-10 10:32:24 -0500

    Done with Domo A

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
+
+const router = require('./router.js');
+
+const port = process.env.PORT || process.env.PORT || 3000;
+
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/Domomaker';
+mongoose.connect(dbURI).catch((err) => {
+  if (err) {
+    console.log(err, ' - Could not connect to database');
+    throw err;
+  }
+});
+
+const app = express();
+app.use(helmet());
+app.use('/assets', express.static(path.resolve(`${__dirname}, '../hosted/`)));
+app.use(favicon(`${__dirname}, '../hosted/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}, '/../views`);
+router(app);
+app.listen(port, (err) => {
+  if (err) { throw err; }
+  console.log(`Server is running on port ${port}`);
+});
