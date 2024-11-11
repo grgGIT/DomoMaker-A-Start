d2fd2b5606bc971d7aee1590dca830234c050fdf
Geoffrey Gracia <grg7576@rit.edu>
2024-11-11 13:53:48 -0500

    favicon update2

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ redisClient.on('error', (err) => console.log('Redis Client Error', err));
 const app = express();
 app.use(helmet());
 app.use('/assets', express.static(path.resolve(`${__dirname}, '../hosted/`)));
-app.use(favicon(`${__dirname}, '../hosted/img/favicon.png`));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
