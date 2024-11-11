95cd05b8d44797838fb3ede6aa9cabd01ab7d0c6
Geoffrey Gracia <grg7576@rit.edu>
2024-11-11 14:13:00 -0500

    fixed routing issue

--- server/router.js
+++ server/router.js
@@ -11,7 +11,7 @@ const router = (app) => {
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
   app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
-  app.get('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
